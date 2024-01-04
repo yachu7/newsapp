1f0dd176f98d0e820ca762396d1ac6fad1d28f9d
yachu7 <yachumaharjan@gmail.com>
2024-01-04 19:49:47 +0545

    icon , dynamic title , card update

--- src/components/News.js
+++ src/components/News.js
@@ -1,94 +1,83 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 export default class News extends Component {
-  static defaultProps ={
-    pageSize : 6,
-    category: "general"
-
-  }
+  static defaultProps = {
+    pageSize: 6,
+    category: "general",
+  };
   static propTypes = {
-    pageSize : PropTypes.number,
-    category: PropTypes.string
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-  }
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `Khabar - ${this.capitalizeFirstLetter(
+      this.props.category
+    )}`;
   }
 
-  async componentDidMount() {
-    let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=6a831f8c589b4ec7ac093e1b12b433c0&page=1&pageSize=${this.props.pageSize} `;
+  async updateNow() {
+    let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=6a831f8c589b4ec7ac093e1b12b433c0&page=${this.state.page}&pageSize=${this.props.pageSize} `;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading : false
+      loading: false,
     });
   }
-  handlePreClick = async () => {
-    
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=6a831f8c589b4ec7ac093e1b12b433c0&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
 
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading : false
-    });
+  async componentDidMount() {
+    this.updateNow();
+  }
+  handlePreClick = async () => {
+    await this.setState({ page: this.state.page - 1 });
+    this.updateNow();
   };
   handleNextClick = async () => {
-    if (
-      !(this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize))
-    ) {
-      
-      
-
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=6a831f8c589b4ec7ac093e1b12b433c0&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize} `;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading : false
-      });
-    }
+    await this.setState({ page: this.state.page + 1 });
+    this.updateNow();
   };
   render() {
     return (
       <div className="container my-3">
-        <h2>Top Headlines</h2>
+        <h2>
+          {this.props.category === "general"
+            ? "Top Headlines"
+            : `Top ${this.capitalizeFirstLetter(
+                this.props.category
+              )} Headlines`}
+        </h2>
         {this.state.loading && <Spinner />}
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageURL={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imageURL={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,43 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description, imageURL, newsUrl } = this.props;
-    const defaultImageURL = "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+    let { title, description, imageURL, newsUrl, author, date, source } =
+      this.props;
+    const defaultImageURL =
+      "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
     return (
       <div>
         <div className="card my-3">
-          <img src={imageURL || defaultImageURL} className="card-img-top" alt="..." style={{height : "200px ",objectFit: "contain" }}/>
+          <span
+            className="position-absolute top-0  translate-middle badge rounded-pill bg-info"
+            style={{
+              left: "80%",
+              zIndex: "1",
+            }}
+          >
+            {source.name}
+          </span>
+
+          <img
+            src={imageURL || defaultImageURL}
+            className="card-img-top"
+            alt="..."
+            style={{ height: "200px ", objectFit: "contain" }}
+          />
           <div className="card-body">
             <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                Last updated on {new Date(date).toGMTString()}{" "}
+              </small>
+            </p>
+
             <a
               href={newsUrl}
               className="stretched-link"
@@ -19,10 +47,11 @@ export default class NewsItem extends Component {
                 color: "inherit", // Use the default text color
                 cursor: "pointer", // Show a pointer cursor on hover
               }}
-            ><span className="visually-hidden">Read more...</span></a>
+            >
+              <span className="visually-hidden">Read more...</span>
+            </a>
           </div>
         </div>
-      
       </div>
     );
   }
